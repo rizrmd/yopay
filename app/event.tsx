3d69688c2d4aa7e8a848139131e1697be3715cd6
rizrmd <rizky@avolut.com>
2024-08-06 13:17:23 +0000

    fix

--- app/event.tsx
+++ app/event.tsx
@@ -2,6 +2,6 @@ import { prasi_events } from "lib/exports";
 
 prasi_events("tablelist", "where", async (table, where) => {
   if (!["customer_track", "bundle_product", "sales_lines"].includes(table)) {
-    where.deleted_at = { isNot: null };
+    where.deleted_at = { not: null };
   }
 });
