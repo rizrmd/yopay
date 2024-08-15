b5fa57c5910041622195ca397fd79eef8aab455f
rizky <rizky@avolut.com>
2024-08-14 21:44:49 -0700

    fix

--- index.tsx
+++ index.tsx
@@ -1,8 +1,9 @@
 import "@/utils/init";
 import "app/css/build.css";
 import "app/event";
+import { lang } from "lib/lang/lang";
 export * from "@/exports";
 export { icon } from "app/icon";
 export { layoutStyle } from "app/layout-style";
 
-  
+await lang.init("id");
