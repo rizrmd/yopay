8e1e08db1dfaa384d914e968a3df3f07641491d3
rizky <rizky@avolut.com>
2024-10-08 00:39:10 -0700

    checkpoint

--- app/server/router/download.ts
+++ app/server/router/download.ts
@@ -1,26 +1,33 @@
-import { trxSalesStatus } from "app/enums";
+import { prasiApi } from "lib/server/server-route";
 import { SessionContext } from "lib/session/type";
 import { EsensiSession } from "../session";
 
-export default async function (
+export default prasiApi(async function (
   this: SessionContext<EsensiSession>,
   id_product: string,
   id_sales: string
 ): Promise<any> {
+  console.log("download", id_product, id_sales);
   const { req, server } = this;
-  console.log('ip address', server.requestIP(req)?.address);
-  console.log('api/download', id_product, id_sales)
+  console.log("ip address", server.requestIP(req)?.address);
+  console.log("api/download", id_product, id_sales);
+
   const product = await db.product.findFirst({
-    where: { id: id_product },
+    where: {
+      id: id_product,
+    },
+    select: {
+      product_file: true,
+    },
   });
-  console.log('product', product)
-  if (product) {
-    // await db.t_sales_download.create({
-    //   data: {
-    //     id_product,
-    //     id_sales,
-    //   },
-    // });
-  }
-  return { hello: "world" };
-}
+  if (!product) return { ok: false, download: [] };
+  const parsedArray = JSON.parse(product.product_file) as string[];
+  await db.t_sales_download.create({
+    data: {
+      id_product,
+      id_sales,
+      ip_address: server.requestIP(req)?.address ?? ''
+    },
+  });
+  return { ok: true, download: parsedArray };
+});
