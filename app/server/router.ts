d43b7a13cb99c617bfa94586f383e2a306f5693b
Rizky <rizky@avolut.com>
2024-12-01 09:50:05 +0000

    fix midtrans

--- app/server/router.ts
+++ app/server/router.ts
@@ -1,6 +1,7 @@
 import { newServerRouter } from "lib/server/server-route";
 
 export const router = newServerRouter({
+  midtrans: ["/_midtrans", () => import("./router/midtrans")],
   register: ["/register", () => import("./router/register")],
   login: ["/api/login", () => import("./router/login")],
   otp: ["/send-otp", () => import("./router/send-otp")],
@@ -9,12 +10,32 @@ export const router = newServerRouter({
   paymentSuccess: ["/pay/success", () => import("./router/payment/success")],
   paymentPending: ["/pay/pending", () => import("./router/payment/pending")],
   paymentError: ["/pay/error", () => import("./router/payment/error")],
-  trxSalesGetNotPaid: ["/api/trx-sales/get/not-paid", () => import("./router/trx-sales/get/not-paid")],
-  trxSalesGetAllPaid: ["/api/trx-sales/get/all-paid", () => import("./router/trx-sales/get/all-paid")],
-  trxSalesCreate: ["/api/trx-sales/create", () => import("./router/trx-sales/create")],
-  trxSalesUpdate: ["/api/trx-sales/update", () => import("./router/trx-sales/update")],
-  otherProducts: ["/api/products/others", () => import("./router/products/others")],
-  productsByCategory: ["/api/products/category", () => import("./router/products/category")],
-  productsByCategorySlug: ["/api/products/category-slug", () => import("./router/products/category-slug")],
+  trxSalesGetNotPaid: [
+    "/api/trx-sales/get/not-paid",
+    () => import("./router/trx-sales/get/not-paid"),
+  ],
+  trxSalesGetAllPaid: [
+    "/api/trx-sales/get/all-paid",
+    () => import("./router/trx-sales/get/all-paid"),
+  ],
+  trxSalesCreate: [
+    "/api/trx-sales/create",
+    () => import("./router/trx-sales/create"),
+  ],
+  trxSalesUpdate: [
+    "/api/trx-sales/update",
+    () => import("./router/trx-sales/update"),
+  ],
+  otherProducts: [
+    "/api/products/others",
+    () => import("./router/products/others"),
+  ],
+  productsByCategory: [
+    "/api/products/category",
+    () => import("./router/products/category"),
+  ],
+  productsByCategorySlug: [
+    "/api/products/category-slug",
+    () => import("./router/products/category-slug"),
+  ],
 });
-   
---
+++ app/server/router/midtrans.ts
@@ -0,0 +1,10 @@
+import { SessionAuth, SessionContext } from "lib/session/type";
+import { EsensiSession } from "../session";
+
+export default async function (this: SessionContext<EsensiSession>) {
+  return {
+    _midtrans_clientKey: process.env._midtrans_clientKey,
+    _midtrans_serverKey: process.env._midtrans_serverKey,
+    _midtrans_base64AuthHeader: process.env._midtrans_base64AuthHeader,
+  };
+}
